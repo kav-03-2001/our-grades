ec97dc9a607ef288fde5a5e1818e932486c17adb
kav-03-2001 <78249332+kav-03-2001@users.noreply.github.com>
2021-01-31 17:02:11 +0530

    Update Nav.js

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -9,12 +9,12 @@ function Nav() {
       <div className="container">
         <h3 className="navbar-header fw-bold" style={{color: "#0000FF"}}>Grades</h3>
         <ul className="nav justify-content-end">
-          <Link to="/"><li className="nav-item nav-link">Home</li></Link>
-          <Link to="/18-277"><li className="nav-item nav-link">18-277</li></Link>
-          <Link to="/18-086"><li className="nav-item nav-link">18-086</li></Link>
-          <Link to="/18-117"><li className="nav-item nav-link">18-117</li></Link>
-          <Link to="/18-236"><li className="nav-item nav-link">18-236</li></Link>
-          <Link to="/18-396"><li className="nav-item nav-link">18-396</li></Link>
+          <Link to="/our-grades"><li className="nav-item nav-link">Home</li></Link>
+          <Link to="/our-grades/18-277"><li className="nav-item nav-link">18-277</li></Link>
+          <Link to="/our-grades/18-086"><li className="nav-item nav-link">18-086</li></Link>
+          <Link to="/our-grades/18-117"><li className="nav-item nav-link">18-117</li></Link>
+          <Link to="/our-grades/18-236"><li className="nav-item nav-link">18-236</li></Link>
+          <Link to="/our-grades/18-396"><li className="nav-item nav-link">18-396</li></Link>
         </ul>
       </div>
     </nav>
